29b7b01a1cbc3b47e6e0355474fe202bfd745173
ibraved <93447338+ibraved@users.noreply.github.com>
2026-05-06 16:09:46 +0300

    Release: pin downstream SHA256 hashes

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -54,8 +54,8 @@ $files = @(
 $ExpectedSha256 = @{
   'run.ps1' = '228517f44fceeddba7af8a7a57176a1c8bbdf2df7facdd0308549a299bcfd6d7'
   'locale.ps1' = '72ae8df69821cb05bd1495ff4d9ec4c554b1ae652622940c0893ed31d390e73b'
-  'czn_event_capture.py' = '1145d3c2837db56ac0c4e148441aee0ba00c84624953f0a9f2de7010136765c4'
-  '_czn_capture_lib.py' = '3a552142605b14c13cf13a52335ff698adf0dd83c9d43105ff37a8c7c34d611f'
+  'czn_event_capture.py' = '98a46cab09f5955e40d2fa2ec2252f89b3f2973fb0399d72db83f1247ce242ed'
+  '_czn_capture_lib.py' = '94e74871c5dc2e5a0cf308fdfba68aef77189b71781db1302ff2f8d76a911920'
   'requirements.txt' = '4d7e04b6adf41622bfc04c1c01c1a1a5541a60c77edd61871a6aff648b6f7dc1'
   'zstd_dictionary.bin' = '8dba3653f67d5b555533c54c17470455d291ef78a3d8b127cd332e0de40383d7'
 }
